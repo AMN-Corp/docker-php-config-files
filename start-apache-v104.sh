e8d81b3f3265fe2d41c8f7b1d45f0481d29ea6eb
Pawat T. <53035209+pawat45884@users.noreply.github.com>
2026-01-20 23:21:11 +0700

    Update DNS resolvers in start-apache-v104.sh
    Removed Docker internal DNS from resolvers setup.

--- start-apache-v104.sh
+++ start-apache-v104.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Ensure DNS resolvers are set (Docker internal DNS first, then public DNS as fallback)
-printf "nameserver 127.0.0.11\nnameserver 8.8.8.8\nnameserver 1.1.1.1\n" > /etc/resolv.conf
+# Ensure DNS resolvers are set
+printf "nameserver 8.8.8.8\nnameserver 1.1.1.1\n" > /etc/resolv.conf
 
 service cron start
 
